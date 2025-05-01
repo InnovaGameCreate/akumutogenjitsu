4c25adf5aa294b9f0c6ec5ecd2f58d2fb107fbda
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-05-01 23:04:10 +0900

    refactor: SpawnEnemyEventのメソッドにコメントを追加

--- Assets/Scripts/GameScene/Event/SpawnEnemyEvent.cs
+++ Assets/Scripts/GameScene/Event/SpawnEnemyEvent.cs
@@ -8,16 +8,21 @@ public class SpawnEnemyEvent : AbstractEvent
     [SerializeField] private Vector2 _position;
 
     private bool _isInEnter;
-
     private bool _hasFinished = false;
 
+    /// <summary>
+    /// 敵をスポーンさせるイベントを実行します。
+    /// </summary>
     public override void TriggerEvent()
     {
         _enemySpawnManager.SpawnEnemy(_position);
-
         _hasFinished = true;
     }
 
+    /// <summary>
+    /// イベントをトリガーする条件を満たしているかを判定します。
+    /// </summary>
+    /// <returns>条件を満たしている場合は true、それ以外は false。</returns>
     public override bool IsTriggerEvent()
     {
         if (EventStatus == eEventStatus.Triggered)
@@ -28,11 +33,19 @@ public class SpawnEnemyEvent : AbstractEvent
         return _isInEnter && (Input.GetKeyDown(KeyCode.Z) || Input.GetKey(KeyCode.Return));
     }
 
+    /// <summary>
+    /// イベントが終了したかどうかを判定します。
+    /// </summary>
+    /// <returns>終了している場合は true、それ以外は false。</returns>
     public override bool IsFinishEvent()
     {
         return _hasFinished;
     }
 
+    /// <summary>
+    /// プレイヤーがトリガー範囲に入った際に呼び出されます。
+    /// </summary>
+    /// <param name="collision">トリガーに入ったオブジェクトのコライダー。</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         _isInEnter = false;
