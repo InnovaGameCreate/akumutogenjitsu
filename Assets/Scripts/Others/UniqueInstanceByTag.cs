8b741f19298e6a4713f7a16933265e8ec8304d18
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-05-25 23:35:46 +0900

    fix: 同じタグのオブジェクトを消すシステムを元からあったオブジェクトを残すように設定

--- Assets/Scripts/Others/UniqueInstanceByTag.cs
+++ Assets/Scripts/Others/UniqueInstanceByTag.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 /// </summary>
 public class UniqueInstanceByTag : MonoBehaviour
 {
-    private void Awake()
+    private bool _isMainObj = false;
+
+    private void Start()
     {
         GameObject[] objects = GameObject.FindGameObjectsWithTag(gameObject.tag);
-        if (objects.Length >= 2)
+        if (objects.Length == 1)
         {
-            Debug.Log($"オブジェクトが2つ以上存在するのでDestroyしました。削除したオブジェクト: {gameObject.transform.name}");
-            Destroy(gameObject);
+            _isMainObj = true;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        foreach (GameObject obj in objects)
+        {
+            if (!obj.GetComponent<UniqueInstanceByTag>().IsMainObj)
+            {
+                Debug.Log($"オブジェクトが2つ以上存在するのでDestroyしました。削除したオブジェクト: {gameObject.transform.name}");
+                Destroy(obj);
+            }
+        }
+    }
+
+    public bool IsMainObj
+    {
+        get
+        {
+            return _isMainObj;
         }
     }
 }
