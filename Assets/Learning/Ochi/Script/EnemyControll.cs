63c6210564f51e3bd92161b0ba90f72d0fa92bc2
ochikondoru <shu331205@gmail.com>
2025-06-18 15:56:50 +0900

    勉強会2まで

--- Assets/Learning/Ochi/Script/EnemyControll.cs
+++ Assets/Learning/Ochi/Script/EnemyControll.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class EnemyControl
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyControl : MonoBehaviour
 {
-    
+    [SerializeField] Transform target;
+    Rigidbody2D rb;
+    float speed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        Vector2 direction = (Vector2)target.position - rb.position;
+
+        direction.Normalize();
+
+        rb.linearVelocity = direction * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("kk");
+            target.name = "test";
+        }
+    }
+
 }
