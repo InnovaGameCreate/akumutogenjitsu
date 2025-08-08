2af101b1b0e1c92c51dbd4477fe643d555fd0a36
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-08-08 21:14:32 +0900

    feat: セーブするデータの名前を設定するメソッド

--- Assets/Scripts/GameScene/System/Save/ISaveData.cs
+++ Assets/Scripts/GameScene/System/Save/ISaveData.cs
@@ -1,6 +1,12 @@
 public interface ISaveData
 {
     /// <summary>
+    /// セーブするデータの名前(JSONの最初になる)
+    /// </summary>
+    /// <returns> 名前 </returns>
+    public string SaveDataName();
+
+    /// <summary>
     /// JSONにエンコードする
     /// </summary>
     /// <returns> JSON </returns>
