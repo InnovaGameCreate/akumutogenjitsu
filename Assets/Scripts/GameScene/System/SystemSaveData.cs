b2a321442c3011069fa7c37fb479f9e633fef08e
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-08-10 00:20:44 +0900

    feat: システム全体のセーブ機能の実装

---
+++ Assets/Scripts/GameScene/System/SystemSaveData.cs
@@ -0,0 +1,32 @@
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class SystemSaveData : ISaveData
+{
+    [JsonProperty("current_scene")]
+    public string CurrentSceneName { get; set; } = "";
+
+    public void DecodeToSaveData(string json)
+    {
+        try
+        {
+            SystemSaveData data = JsonConvert.DeserializeObject<SystemSaveData>(json);
+            this.CurrentSceneName = data?.CurrentSceneName ?? "";
+        }
+        catch (JsonException ex)
+        {
+            Debug.LogError($"Systemのセーブデータの生成に失敗しました。 {ex.Message}");
+            CurrentSceneName = "";
+        }
+    }
+
+    public string EncodeToJson()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+
+    public string SaveDataName()
+    {
+        return "system";
+    }
+}
