69844831c444e97c4d7f5cd9df7546d437832307
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-09-03 14:52:03 +0900

    feat: 日付を超えたらStoryLayerを0に初期化する

--- Assets/Scripts/GameScene/Story/StoryManager.cs
+++ Assets/Scripts/GameScene/Story/StoryManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class StoryManager : Singleton<StoryManager>
 {
     [SerializeField] private int _storyLayer = 1;
-    
+
     /// <summary>
     /// 現在のStoryLayer(1以上)
     /// </summary>
@@ -22,5 +22,10 @@ public class StoryManager : Singleton<StoryManager>
             }
             _storyLayer = value;
         }
+    }
+
+    public void Initialize()
+    {
+        _storyLayer = 0;
     }
 }
