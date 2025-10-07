7b5634ca60363a60f4b5e634f9360a7192828649
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-10-07 21:26:22 +0900

    feat: テストイベントで実装

--- Assets/Scripts/GameScene/Event/TestEvent.cs
+++ Assets/Scripts/GameScene/Event/TestEvent.cs
@@ -7,21 +7,25 @@ public class TestEvent : AbstractEvent
 
     public override void TriggerEvent()
     {
+        Debug.Log("イベントを実行");
     }
 
     public override void OnFinishEvent()
     {
+        Debug.Log("イベントを終了");
     }
 
     public override void OnUpdateEvent()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
+            // イベントを発生させる
             onTriggerEvent.OnNext(Unit.Default);
         }
 
         if (Input.GetKeyDown(KeyCode.B))
         {
+            // イベントを終了させる
             onFinishEvent.OnNext(Unit.Default);
         }
     }
