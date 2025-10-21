100a06307725b53bcacb5ebc8326da3306ad1f44
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-10-22 08:50:21 +0900

    feat: SeEventの実装

---
+++ Assets/Scripts/GameScene/Event/SeEvent/SeEvent.cs
@@ -0,0 +1,30 @@
+using R3;
+using UnityEngine;
+
+public class SeEvent : AbstractEvent
+{
+    [Header("SE")]
+    [SerializeField] private AudioData _seAudioData;
+    [SerializeField] private bool _isTriggerForce = false;
+    private bool _isInEvent;
+    public override void OnStartEvent()
+    {
+        PlayerInput.Instance.Input.Base.Interact.performed += ctx =>
+        {
+            if (ctx.ReadValueAsButton() && _isInEvent)
+            {
+                onTriggerEvent.OnNext(Unit.Default);
+            }
+        };
+
+        if (_isTriggerForce)
+        {
+            onTriggerEvent.OnNext(Unit.Default);
+        }
+    }
+
+    public override void TriggerEvent()
+    {
+        Audio.Instance.PlaySe(_seAudioData);
+    }
+}
